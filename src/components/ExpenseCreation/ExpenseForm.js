b15ff1f2e4aaf7e64acd2aa6f708484137ee23aa
Ryuytyuu-Greseven <rukku93223@gmail.com>
2023-07-28 23:56:06 +0530

    Added Multuple States for User Adding Expense form!

--- src/components/ExpenseCreation/ExpenseForm.js
+++ src/components/ExpenseCreation/ExpenseForm.js
@@ -1,20 +1,39 @@
+import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [newTitle, setTitleState] = useState("");
+  const [newAmount, setAmountState] = useState("");
+  const [newDate, setDateState] = useState("");
+
+  const titleChangeHandler = (event) => {
+    console.log(event.target.value);
+    setTitleState(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    console.log(event.target.value);
+    setAmountState(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    console.log(event.target.value);
+    setDateState(event.target.value);
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" />
+          <input type="text" onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" />
+          <input type="number" onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" />
+          <input type="date" onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
