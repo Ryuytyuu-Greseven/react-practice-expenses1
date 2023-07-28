8550b91c83d18acf4ea25a67b9f7d367487d351d
Ryuytyuu-Greseven <rukku93223@gmail.com>
2023-07-29 00:40:21 +0530

    Added Previous state accessing functionalty with shared Handler.

--- src/components/ExpenseCreation/ExpenseForm.js
+++ src/components/ExpenseCreation/ExpenseForm.js
@@ -2,22 +2,68 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  const [newTitle, setTitleState] = useState("");
-  const [newAmount, setAmountState] = useState("");
-  const [newDate, setDateState] = useState("");
+  // multiple states
+  // const [newTitle, setTitleState] = useState("");
+  // const [newAmount, setAmountState] = useState("");
+  // const [newDate, setDateState] = useState("");
+  // const titleChangeHandler = (event) => {
+  //   console.log(event.target.value);
+  //   setTitleState(event.target.value);
+  // };
+  // const amountChangeHandler = (event) => {
+  //   console.log(event.target.value);
+  //   setAmountState(event.target.value);
+  // };
+  // const dateChangeHandler = (event) => {
+  //   console.log(event.target.value);
+  //   setDateState(event.target.value);
+  // };
 
-  const titleChangeHandler = (event) => {
-    console.log(event.target.value);
-    setTitleState(event.target.value);
-  };
+  const [newExenseDetails, setNewExpenseDetails] = useState({
+    expenseTitle: "",
+    expenseAmount: "",
+    expenseDate: "",
+  });
+  // const titleChangeHandler = (event) => {
+  //   // setNewExpenseDetails({
+  //   //   ...newExenseDetails,
+  //   //   expenseTitle: event.target.value,
+  //   // });
 
-  const amountChangeHandler = (event) => {
-    console.log(event.target.value);
-    setAmountState(event.target.value);
-  };
-  const dateChangeHandler = (event) => {
-    console.log(event.target.value);
-    setDateState(event.target.value);
+  //   setNewExpenseDetails((previousState) => {
+  //     return { ...previousState, expenseTitle: event.target.value };
+  //   });
+  // };
+  // const amountChangeHandler = (event) => {
+  //   // setNewExpenseDetails({
+  //   //   ...newExenseDetails,
+  //   //   expenseAmount: event.target.value,
+  //   // });
+  //   setNewExpenseDetails((previousState) => {
+  //     return { ...previousState, expenseAmount: event.target.value };
+  //   });
+  // };
+  // const dateChangeHandler = (event) => {
+  //   // setNewExpenseDetails({
+  //   //   ...newExenseDetails,
+  //   //   expenseDate: event.target.value,
+  //   // });
+  //   setNewExpenseDetails((previousState) => {
+  //     return { ...previousState, expenseDate: event.target.value };
+  //   });
+  // };
+
+  const sharedFormUpdateHandler = (type, value) => {
+    console.log(type, value);
+    setNewExpenseDetails((previousState) => {
+      if (type === "title") {
+        return { ...previousState, expenseTitle: value };
+      } else if (type === "amount") {
+        return { ...previousState, expenseAmount: value };
+      } else {
+        return { ...previousState, expenseDate: value };
+      }
+    });
   };
 
   return (
@@ -25,15 +71,30 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input
+            type="text"
+            onChange={(event) => {
+              sharedFormUpdateHandler("title", event.target.value);
+            }}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" onChange={amountChangeHandler} />
+          <input
+            type="number"
+            onChange={(event) => {
+              sharedFormUpdateHandler("amount", event.target.value);
+            }}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" onChange={dateChangeHandler} />
+          <input
+            type="date"
+            onChange={(event) => {
+              sharedFormUpdateHandler("date", event.target.value);
+            }}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
