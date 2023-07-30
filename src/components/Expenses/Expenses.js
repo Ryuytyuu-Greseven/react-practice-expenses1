bb45e8e61e3b27907228dc7b0864d595a3bc5666
Ryuytyuu-Greseven <rukku93223@gmail.com>
2023-07-30 23:46:24 +0530

    Added filter with state lifting, yet the filtering functionality is pending!

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import Card from "../UI/Cards/Card";
+import ExpenseFilter from "./ExpenseFilter/ExpenseFilter";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import "./Expenses.css";
 
 function Expenses(props) {
+  const [selectedYear, updateFilterYear] = useState("2019");
+  const filterExpensesByYear = (event) => {
+    console.log(event);
+    updateFilterYear(event);
+  };
+
   return (
     <Card className="expenses">
+      <ExpenseFilter
+        filterYear={selectedYear}
+        filterExpenses={filterExpensesByYear}
+      />
       {props.expenses.map((singleExpense) => (
         <ExpenseItem expense={singleExpense}></ExpenseItem>
       ))}
