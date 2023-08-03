1b09b796c614ea11ebb18256b7b2f3e0d2bd2b3d
Ryuytyuu-Greseven <rukku93223@gmail.com>
2023-08-04 00:22:15 +0530

    Added useref for inut fields!

--- src/components/Project2/Users/AddUser.js
+++ src/components/Project2/Users/AddUser.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button2 from "../UI/Button/Button2";
 import Card2 from "../UI/Card/Card2";
 
@@ -7,37 +7,53 @@ import ErrorModal from "../UI/ErrorModal/ErrorModal";
 import Wrapper from "../../Helpers/Wrapper";
 
 const AddUser = (props) => {
-  const [username, setUserName] = useState("");
-  const [userage, setUserAge] = useState("");
+  // const [username, setUserName] = useState("");
+  // const [userage, setUserAge] = useState("");
+
+  // use refs
+  const userNameEntered = useRef();
+  const userAgeEntered = useRef();
 
   // error modal states
   const [displayErrorModal, setError] = useState(false);
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (!username.length || !userage.length || +userage < 1) {
+    console.log(userNameEntered, userAgeEntered);
+    if (
+      !userNameEntered.current.value.length ||
+      !userAgeEntered.current.value.length ||
+      +userAgeEntered.current.value < 1
+    ) {
       setError({
         title: "Invalid Input",
         message: "Please enter valid details",
       });
       return false;
     }
-    console.log(username, userage);
+    console.log(userNameEntered.current.value, userAgeEntered.current.value);
+
     const newUser = {
-      username,
-      userage,
+      username: userNameEntered.current.value,
+      userage: userAgeEntered.current.value,
     };
     props.addUser(newUser);
-    setUserAge("");
-    setUserName("");
+
+    // reset ref values
+    userNameEntered.current.value = "";
+    userAgeEntered.current.value = "";
+
+    // resetting the state
+    // setUserAge("");
+    // setUserName("");
   };
 
-  const ageChangeHandler = (event) => {
-    setUserAge(event.target.value);
-  };
-  const usernameChangeHandler = (event) => {
-    setUserName(event.target.value);
-  };
+  // const ageChangeHandler = (event) => {
+  //   setUserAge(event.target.value);
+  // };
+  // const usernameChangeHandler = (event) => {
+  //   setUserName(event.target.value);
+  // };
 
   // error modal state udpates
   const closeErrorModal = () => {
@@ -60,15 +76,17 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={username}
-            onChange={usernameChangeHandler}
+            // value={username}
+            // onChange={usernameChangeHandler}
+            ref={userNameEntered}
           />
           <label htmlFor="age">Age Years</label>
           <input
             id="age"
             type="number"
-            value={userage}
-            onChange={ageChangeHandler}
+            // value={userage}
+            // onChange={ageChangeHandler}
+            ref={userAgeEntered}
           />
           <Button2 type="submit">Add User</Button2>
         </form>
